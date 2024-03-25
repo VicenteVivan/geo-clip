a60be5884b21534260c4e16ca1deea278b678f1d
Vicente Vivanco Cepeda <vicentevivanco@knights.ucf.edu>
2024-03-25 14:14:09 -0400

    Correct GPS Queue

--- geoclip/model/GeoCLIP.py
+++ geoclip/model/GeoCLIP.py
@@ -45,40 +45,24 @@ class GeoCLIP(nn.Module):
         self.register_buffer("gps_queue_ptr", torch.zeros(1, dtype=torch.long))
 
     @torch.no_grad()
-    def _dequeue_and_enqueue(self, gps):
+    def dequeue_and_enqueue(self, gps):
         """ Update GPS queue
 
         Args:
             gps (torch.Tensor): GPS tensor of shape (batch_size, 2)
         """
-        opt = self.opt
         gps_batch_size = gps.shape[0]
-        batch_size = opt.batch_size
-
         gps_ptr = int(self.gps_queue_ptr)
         
-        assert self.queue_size % batch_size == 0
+        assert self.queue_size % gps_batch_size == 0, f"Queue size {self.queue_size} should be divisible by batch size {gps_batch_size}"
 
-        # Replace the GPS from ptr to ptr+batch_size (dequeue and enqueue)
+        # Replace the GPS from ptr to ptr+gps_batch_size (dequeue and enqueue)
         self.gps_queue[:, gps_ptr:gps_ptr + gps_batch_size] = gps.t()
-        gps_ptr = (gps_ptr + batch_size) % self.queue_size  # move pointer
+        gps_ptr = (gps_ptr + gps_batch_size) % self.queue_size  # move pointer
         self.gps_queue_ptr[0] = gps_ptr
 
-    def append_gps_queue_features(self, gps_features):
-        """ Compute the GPS queue features and append them to the given GPS features."""
-
-        # Get the GPS queue features
-        location_queue = self.gps_queue.t().detach()
-        gps_queue_features = self.location_encoder(location_queue)
-        gps_queue_features = F.normalize(gps_queue_features, dim=1)
-
-        # Concatenate Features (GPS Features & GPS Queue Features)
-        gps_features = torch.cat([gps_features, gps_queue_features], dim=0)
-
-        # Update GPS queue
-        self._dequeue_and_enqueue(gps)
-
-        return gps_features
+    def get_gps_queue(self):
+        return self.gps_queue.t()
                                              
     def forward(self, image, location):
         """ GeoCLIP's forward pass
@@ -86,6 +70,9 @@ class GeoCLIP(nn.Module):
         Args:
             image (torch.Tensor): Image tensor of shape (n, 3, 224, 224)
             location (torch.Tensor): GPS location tensor of shape (m, 2)
+
+        Returns:
+            logits_per_image (torch.Tensor): Logits per image of shape (n, m)
         """
 
         # Compute Features
@@ -99,9 +86,8 @@ class GeoCLIP(nn.Module):
         
         # Cosine similarity (Image Features & Location Features)
         logits_per_image = logit_scale * (image_features @ location_features.t())
-        logits_per_location = logits_per_image.t()
 
-        return logits_per_image, logits_per_location
+        return logits_per_image
 
     @torch.no_grad()
     def predict(self, image_path, top_k):
@@ -110,6 +96,10 @@ class GeoCLIP(nn.Module):
         Args:
             image_path (str): Path to the image
             top_k (int): Number of top predictions to return
+
+        Returns:
+            top_pred_gps (torch.Tensor): Top k GPS coordinates of shape (k, 2)
+            top_pred_prob (torch.Tensor): Top k GPS probabilities of shape (k,)
         """
         image = Image.open(image_path)
         image = self.image_encoder.preprocess_image(image)
@@ -117,10 +107,10 @@ class GeoCLIP(nn.Module):
 
         gps_gallery = self.gps_gallery.to(self.device)
 
-        logits_per_image, logits_per_location = self.forward(image, gps_gallery)
+        logits_per_image = self.forward(image, gps_gallery)
         probs_per_image = logits_per_image.softmax(dim=-1).cpu()
 
-        # Get top k prediction
+        # Get top k predictions
         top_pred = torch.topk(probs_per_image, top_k, dim=1)
         top_pred_gps = self.gps_gallery[top_pred.indices[0]]
         top_pred_prob = top_pred.values[0]
